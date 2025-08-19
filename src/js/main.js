e17ea2587cf0d19ff2286d0095c2beacc2563398
FilipchikAV <im0602060@gmail.com>
2025-08-19 18:45:59 +0900

    мобильня версия почти готова

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 let btnMobile = document.querySelector('.header__btn');
 let navList = document.querySelector('.header__list');
+let headerLink = document.querySelectorAll('.header__link');
 
 btnMobile.addEventListener('click', toggleMenu);
+
+headerLink.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
 
 function toggleMenu() {
   navList.classList.toggle('visually-hidden');
