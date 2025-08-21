604236101b79893381954165824855b2e16556bc
FilipchikAV <im0602060@gmail.com>
2025-08-21 17:41:16 +0900

    сделал версию мобильную и планшетную

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,42 @@ headerLink.forEach(link => {
 });
 
 function toggleMenu() {
-  navList.classList.toggle('visually-hidden');
+  navList.classList.toggle('header__list--visually-hidden');
   btnMobile.classList.toggle('header__btn--vert');
 }
+
+let prevButton = document.querySelector('.works__btn--prev');
+let nextButton = document.querySelector('.works__btn--next');
+let slides = Array.from(document.querySelectorAll('.works__item'));
+let slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.remove('visually-hidden');
+    } else {
+      slide.classList.add('visually-hidden');
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
